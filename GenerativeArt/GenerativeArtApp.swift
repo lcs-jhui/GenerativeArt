ff780c692dff62533539b144d832b68864748a05
Justin Hui <jhui@lcs.on.ca>
2023-10-24 11:30:56 -0400

    Initial Commit

---
+++ GenerativeArt/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  GenerativeArt
+//
+//  Created by Justin Hui on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
