f22c808527efa7d8f12e8c3b8dd8e35e4d9b3457
Justin Hui <jhui@lcs.on.ca>
2023-10-24 12:03:08 -0400

    Added triangle bottom right

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -8,6 +8,26 @@
 import SwiftUI
 
 
+struct TriangleBottomRight: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        //Make an empty path
+        var path = Path()
+        
+        //Define the path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) // 1
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY)) // 2
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY)) // 3
+        
+        //Return the path
+        return path
+        
+    }
+    
+}
+
 
 struct TriangleTopLeft: Shape {
     func path(in rect: CGRect) -> Path {
@@ -76,7 +96,7 @@ struct TileView: View {
         VStack {
             
             //Top right triangle
-            TriangleTopLeft()
+            TriangleBottomRight()
                 //Make the shape be a square
                 .aspectRatio(1.0, contentMode: .fit)
             
