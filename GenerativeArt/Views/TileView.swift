a494b59135c96c62fbba1b7959d4aff29cf965f1
Justin Hui <jhui@lcs.on.ca>
2023-10-26 14:46:03 -0400

    Added code to implement colour selection according to the algorithm

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -13,7 +13,32 @@ struct TileView: View {
     //MARK: Stored Properties
     let flipOne = Coin.flip()
     
+    //Choosing marker colours
+    let markerOne = Color.red
+    let markerTwo = Color.blue
+    
+    //Decide on colors
+    let flipForColor = Coin.heads
+    
     //MARK: Computed Properties
+    
+    //Set colours based on the coin flip
+    var colourOne: Color {
+        if flipForColor == .heads {
+            return markerOne
+        } else {
+            return markerTwo
+        }
+    }
+    
+    var colourTwo: Color {
+        if flipForColor == .heads {
+            return markerTwo
+        } else {
+            return markerOne
+        }
+    }
+    
     var body: some View {
         ZStack {
             
@@ -22,13 +47,13 @@ struct TileView: View {
                 //Top Right triangle
                 TriangleTopRight()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colourOne)
                     .aspectRatio(1.0, contentMode: .fit)
                 
                 //Bottom Left triangle
                 TriangleBottomLeft()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colourTwo)
                     .aspectRatio(1.0, contentMode: .fit)
                 
             } else {
@@ -36,13 +61,13 @@ struct TileView: View {
                 //Top Left triangle
                 TriangleTopLeft()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colourOne)
                     .aspectRatio(1.0, contentMode: .fit)
                 
                 //Bottom Right Triangle
                 TriangleBottomRight()
                     .stroke(.black)
-                    .fill(.clear)
+                    .fill(colourTwo)
                     .aspectRatio(1.0, contentMode: .fit)
                 
             }
